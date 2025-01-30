89329bbff9f08b68a069a1b7b288f259f3a10ca1
Sophia Klatell <sklatell@smith.edu>
2025-01-30 12:05:16 -0500

    Just adding messages

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
 def main():
   print("Hello, CSC120-2025 (I am hungry fun fact!")
+  
+  print("There are many many changes that need to be made to properly get in the process of making changes")
   x = 5 + 5
   print("5 + 5 = ", x)
   
